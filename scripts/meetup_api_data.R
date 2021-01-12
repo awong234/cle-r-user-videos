1e3e4e2a5cbb2c655e05d87f8d66289d59fb8066
Alec Wong <aw685@cornell.edu>
2021-01-11 21:12:35 -0500

    Removing separate scripts in favor of one

--- scripts/meetup_api_data.R
+++
@@ -1,8 +0,0 @@
-# Setup ------------------------------------------------------------------------
-
-source('libraries.R')
-source('R/functions.R')
-
-# Meetup API; get past events --------------------------------------------------
-
-meetup_data = get_meetup_metadata()
