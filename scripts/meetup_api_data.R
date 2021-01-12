5d5a81f731680f1dbd16137cc539818d379bfbfd
Alec Wong <aw685@cornell.edu>
2021-01-11 21:12:20 -0500

    Update separate scripts

--- scripts/meetup_api_data.R
+++ scripts/meetup_api_data.R
@@ -1,35 +1,8 @@
 # Setup ------------------------------------------------------------------------
 
-library(jsonlite)
-library(dplyr)
-library(rvest)
-library(keyring)
-library(reticulate)
+source('libraries.R')
+source('R/functions.R')
 
 # Meetup API; get past events --------------------------------------------------
 
-url = "https://api.meetup.com/Cleveland-UseR-Group/events/?&status=past"
-html = read_html(url)
-content = html %>%
-    # Get the text out
-    xml2::xml_find_all('//text()') %>%
-    # Convert to character
-    as.character() %>% paste0(collapse = '') %>%
-    # Convert from character json to list
-    parse_json()
-
-dates = lapply(content, function(x) x$local_date) %>% do.call(c, .) %>% as.Date()
-names = lapply(content, function(x) x$name) %>% do.call(c, .)
-links = lapply(content, function(x) x$link) %>% do.call(c, .)
-desc  = lapply(content, function(x) x$description) %>% do.call(c, .)
-
-df = tibble(
-    names,
-    dates,
-    links,
-    desc
-)
-
-# Remove the cafe's
-df = df %>%
-    filter(names != 'Virtual R Café')
+meetup_data = get_meetup_metadata()
--- scripts/youtube_api.R
+++ scripts/youtube_api.R
@@ -5,52 +5,12 @@ library(xml2)
 library(jsonlite)
 library(purrr)
 
-
-# Youtube API -- get the videos ------------------------------------------------
+source('R/functions.R')
 
 rusergroup_channel_id = URLencode('UC7C4YZ-9itQW7Nl4RVKDflg', reserved = TRUE)
 key = Sys.getenv("APIKEY")
 
-format_response = function(url) {
-    read_html(url) %>%
-        xml2::xml_find_all('//text()') %>%
-        as.character() %>%
-        parse_json()
-}
-
-get_videos_list = function(baseurl) {
-    get_item_detail = function(resp, detail) {
-        out = map(resp$items, ~.x[[detail]])
-        out[sapply(out, is.null)] = NA_character_
-        out = do.call(c, out)
-        return(out)
-    }
-    resp          = format_response(url)
-    titles        = get_item_detail(resp, c('snippet', 'title'))
-    publish_times = get_item_detail(resp, c('snippet', 'publishTime'))
-    video_ids     = get_item_detail(resp, c('id', 'videoId'))
-    kinds         = get_item_detail(resp, c('id', 'kind'))
-    while(!is.null(resp$nextPageToken)) {
-        nextToken     = resp$nextPageToken
-        modify_url    = paste0(url, sprintf("&pageToken=%s", nextToken))
-        resp          = format_response(modify_url)
-        titles        = c(titles, get_item_detail(resp, c('snippet', 'title')))
-        publish_times = c(publish_times, get_item_detail(resp, c('snippet', 'publishTime')))
-        video_ids     = c(video_ids, get_item_detail(resp, c('id', 'videoId')))
-        kinds         = c(kinds, get_item_detail(resp, c('id', 'kind')))
-    }
-    df = data.frame(
-        titles        = titles,
-        publish_times = publish_times,
-        video_ids     = video_ids
-    )
-    video_links = paste0('http://www.youtube.com/watch?v=', video_ids)
-    df$links = video_links
-    # keep only the videos
-    df = df[kinds == 'youtube#video', ]
-
-    return(df)
-}
+# Youtube API -- get the videos ------------------------------------------------
 
 parts = c('snippet,id')
 url = sprintf('https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=%s',
