f30fbb01781874b9102779ddc3d31c9559110f0c
Alec Wong <aw685@cornell.edu>
2021-01-11 21:15:49 -0500

    Update data pull in main script

--- scripts/get_video_list.R
+++ scripts/get_video_list.R
@@ -3,6 +3,9 @@
 source('libraries.R')
 source('R/functions.R')
 
+# Memoise so as to avoid repetitive API calls when not required
+get_youtube_videos_list = memoise::memoise(f = get_youtube_videos_list, cache = memoise::cache_filesystem(path = 'cache'))
+
 # Data pull --------------------------------------------------------------------
 
 # Meetup
@@ -14,11 +17,11 @@ meetup_data = get_meetup_metadata()
 rusergroup_channel_id = URLencode('UC7C4YZ-9itQW7Nl4RVKDflg', reserved = TRUE)
 key = Sys.getenv("APIKEY")
 parts = c('snippet,id')
-url = sprintf('https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=%s',
+baseurl = sprintf('https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=%s',
               key,
               rusergroup_channel_id,
               parts)
 
-youtube_vids = get_youtube_videos_list()
+youtube_vids = get_youtube_videos_list(baseurl)
 
 # Merge data
