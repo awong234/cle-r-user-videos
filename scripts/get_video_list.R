f13f588d6f7d4a29104ae9cd625c9a9e1451d7e5
Alec Wong <aw685@cornell.edu>
2021-01-12 21:49:37 -0500

    Update video script -- matches are passable, but not perfect

--- scripts/get_video_list.R
+++ scripts/get_video_list.R
@@ -10,7 +10,7 @@ get_youtube_videos_list = memoise::memoise(f = get_youtube_videos_list, cache = 
 
 # Meetup
 
-meetup_data = get_meetup_metadata()
+meetup_data = get_meetup_metadata() %>% filter(names != 'Virtual R Café')
 
 # Youtube
 
@@ -24,4 +24,36 @@ baseurl = sprintf('https://www.googleapis.com/youtube/v3/search?key=%s&channelId
 
 youtube_vids = get_youtube_videos_list(baseurl)
 
-# Merge data
+# Merge data -- the names are similar but not exact. Bigram matching should do the trick.
+
+meetup_data_ngrams = meetup_data %>%
+    mutate(names_mod = gsub(pattern = '\\d', replacement = '', x = names, perl = TRUE)) %>%
+    tidytext::unnest_ngrams(output = 'ngram', input = names_mod, n = 2, format = 'text', to_lower = TRUE, stopwords = c(stop_words$word))
+
+youtube_vids_ngrams = youtube_vids %>%
+    mutate(titles_mod = gsub(pattern = '\\d', replacement = '', x = titles, perl = TRUE)) %>%
+    tidytext::unnest_ngrams(output = 'ngram', input = titles_mod, n = 2, format = 'text', to_lower = TRUE, stopwords = c(stop_words$word))
+
+meetup_data_ngrams_spl = meetup_data_ngrams %>% group_by(names) %>% group_split()
+youtube_vids_ngrams_spl = youtube_vids_ngrams %>% group_by(video_ids) %>% group_split()
+
+# For each ngram in the meetups, search the ngrams in the youtube vids, and tally matches
+meetup_names = sapply(meetup_data_ngrams_spl, function(x) x$names[1])
+youtube_titles = sapply(youtube_vids_ngrams_spl, function(x) x$titles[1])
+vid_records = list()
+for (v in seq_along(youtube_vids_ngrams_spl)) {
+    ngrams = youtube_vids_ngrams_spl[[v]][['ngram']]
+    meetup_matches = vector(mode = 'integer', length = length(meetup_names))
+    names(meetup_matches) = meetup_names
+    for (m in seq_along(meetup_data_ngrams_spl)) {
+        meetup_ngrams = meetup_data_ngrams_spl[[m]][['ngram']]
+        meetup_matches[m] = sapply(ngrams, function(x) x == meetup_ngrams) %>% sum
+    }
+    vid_records[[v]] = meetup_matches
+}
+
+vid_matches = sapply(vid_records, which.max)
+tibble(
+    youtube_title = youtube_titles,
+    meetup_name   = meetup_names[vid_matches]
+) %>% print(n=100)
