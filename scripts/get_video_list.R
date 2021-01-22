f0ca587d757ee400d24d7d55231d4bbf240af99a
Alec Wong <aw685@cornell.edu>
2021-01-21 22:14:37 -0500

    Update vid matching method; incomplete but much better

--- scripts/get_video_list.R
+++ scripts/get_video_list.R
@@ -24,36 +24,49 @@ baseurl = sprintf('https://www.googleapis.com/youtube/v3/search?key=%s&channelId
 
 youtube_vids = get_youtube_videos_list(baseurl)
 
-# Merge data -- the names are similar but not exact. Bigram matching should do the trick.
+# Merge data -- the names are similar but not exact. Monogram and bigram matching should do the trick.
 
-meetup_data_ngrams = meetup_data %>%
-    mutate(names_mod = gsub(pattern = '\\d', replacement = '', x = names, perl = TRUE)) %>%
-    tidytext::unnest_ngrams(output = 'ngram', input = names_mod, n = 2, format = 'text', to_lower = TRUE, stopwords = c(stop_words$word))
+meetup_onegrams = tokenize_ngrams(meetup_data$names, n = 1, lowercase = TRUE, stopwords = c(stop_words$word))
+meetup_bigrams = tokenize_ngrams(meetup_data$names, n = 2, lowercase = TRUE)
+youtube_onegrams = tokenize_ngrams(youtube_vids$titles, n = 1, lowercase = TRUE, stopwords = c(stop_words$word))
+youtube_bigrams = tokenize_ngrams(youtube_vids$titles, n = 1, lowercase = TRUE, stopwords = c(stop_words$word))
 
-youtube_vids_ngrams = youtube_vids %>%
-    mutate(titles_mod = gsub(pattern = '\\d', replacement = '', x = titles, perl = TRUE)) %>%
-    tidytext::unnest_ngrams(output = 'ngram', input = titles_mod, n = 2, format = 'text', to_lower = TRUE, stopwords = c(stop_words$word))
-
-meetup_data_ngrams_spl = meetup_data_ngrams %>% group_by(names) %>% group_split()
-youtube_vids_ngrams_spl = youtube_vids_ngrams %>% group_by(video_ids) %>% group_split()
-
-# For each ngram in the meetups, search the ngrams in the youtube vids, and tally matches
-meetup_names = sapply(meetup_data_ngrams_spl, function(x) x$names[1])
-youtube_titles = sapply(youtube_vids_ngrams_spl, function(x) x$titles[1])
-vid_records = list()
-for (v in seq_along(youtube_vids_ngrams_spl)) {
-    ngrams = youtube_vids_ngrams_spl[[v]][['ngram']]
-    meetup_matches = vector(mode = 'integer', length = length(meetup_names))
-    names(meetup_matches) = meetup_names
-    for (m in seq_along(meetup_data_ngrams_spl)) {
-        meetup_ngrams = meetup_data_ngrams_spl[[m]][['ngram']]
-        meetup_matches[m] = sapply(ngrams, function(x) x == meetup_ngrams) %>% sum
+meetup_matches = vector(mode = 'integer', length = length(meetup_onegrams))
+matchorder = cross2(meetup_data$names, youtube_vids$titles)
+meetup_index = map_int(matchorder, ~match(.x[[1]], meetup_data$names))
+matchlist_1 = cross2(meetup_onegrams, youtube_onegrams)
+matchlist_2 = cross2(meetup_bigrams, youtube_bigrams)
+tmp1 = map_int(matchlist_1, function(x) {
+    meetup_grams = x[[1]]
+    youtube_grams = x[[2]]
+    meetup_matches = rep(0, length(meetup_grams))
+    for (i in meetup_grams) {
+        meetup_matches[i] = sum(i == youtube_grams) %>% as.integer()
     }
-    vid_records[[v]] = meetup_matches
+    return(as.integer(sum(meetup_matches)))
+})
+
+tmp2 = map_int(matchlist_2, function(x) {
+    meetup_grams = x[[1]]
+    youtube_grams = x[[2]]
+    meetup_matches = rep(0, length(meetup_grams))
+    for (i in meetup_grams) {
+        meetup_matches[i] = sum(i == youtube_grams)
+    }
+    out = as.integer(sum(meetup_matches))
+    return(out)
+})
+
+matchvalues = (tmp1 + tmp2)
+
+# For each meetup desc, take the greatest number
+maxmatches = vector(mode = integer, length = nrow(meetup_data))
+for (i in seq_along(meetup_matches)) {
+    message(meetup_data$names[i])
+    ind = which(meetup_index == i)
+    this_meetup = matchorder[ind]
+    this_matches = matchvalues[ind]
+    best_match_ind = ind[which.max(this_matches)]
+    print(matchorder[best_match_ind])
 }
 
-vid_matches = sapply(vid_records, which.max)
-tibble(
-    youtube_title = youtube_titles,
-    meetup_name   = meetup_names[vid_matches]
-) %>% print(n=100)
